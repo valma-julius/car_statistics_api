e9132a52608e1e2c6ea4410c733fd62f457852d1
Valma Julius <valma.julius@gmail.com>
2022-11-13 23:54:31 +0200

    last_fixes_for_integration_tests

--- src/backend/carstat/test/auth.e2e-spec.ts
+++ src/backend/carstat/test/auth.e2e-spec.ts
@@ -15,7 +15,7 @@ describe('Authentication System', () => {
     await app.init();
   });
 
-  it('handles a signup request', () => {
+  it('handles a signup request', async () => {
     const _email = 'test@gmail.com';
     return request(app.getHttpServer())
       .post('/auth/signup')
@@ -26,5 +26,23 @@ describe('Authentication System', () => {
         expect(id).toBeDefined();
         expect(email).toEqual(_email);
       });
+  });
+
+  it('signup as new user then get currently logged in user', async () => {
+    const _email = 'test@gmail.com';
+
+    const response = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: _email, password: '12341234' })
+      .expect(201);
+
+    const cookie = response.get('Set-Cookie');
+
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(_email);
   });
 });
